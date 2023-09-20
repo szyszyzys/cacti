49ef73499960b4a5e6b22532c617df37f9a4ea23
Zeyu Song <szyszy0315@gmail.com>
2023-09-19 22:15:43 -0400

    fix

--- scripts/run_exp.sh
+++ scripts/run_exp.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 associativities=$1
-cache_sizes=${2,-"65536 131072 262144 524288 1048576 2097152"}
+cache_sizes=${2:-"65536 131072 262144 524288 1048576 2097152"}
 filename="exp.cfg"
 
 for a in $associativities;
 do
   for c in $cache_sizes;
   do
+      echo current associativity "$a" cache size "$c"
       cp "c.cfg" "$filename"
       echo "-size (bytes) $c" >> "$filename"
       echo "-associativity $a" >> "$filename"
-      ./cacti -infile "$filename" >> log.txt 2>&1
+      ./cacti -infile "$filename" | tee -a ./scripts/res.log
   done
 done
 
